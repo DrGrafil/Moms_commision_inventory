5e1abfb18e44128077a2d0e4b86cfe75ae0cb315
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 22:05:04 -0800

    Update grab_images.py

--- grab_images.py
+++ grab_images.py
@@ -1,10 +1,30 @@
+import os
 import urllib.request
+from urllib.parse import urlparse
+from pathlib import Path
 from Inventory_Item import Inventory_Item
 
 
 def grab_images(all_inventory: [Inventory_Item]) -> bool:
     for item in all_inventory:
-        print(item.image_url)
-        #urllib.request.urlretrieve()
+        file_name = os.path.splitext(os.path.basename(item.image_url))
+        if file_name[0] is '':
+            print("No Image")
+            item.image_location = 'images/no_image.jpg'
+        else:
+            print("File:" + file_name[0] + " ext:" + file_name[1])
+            image_path = None
+            if file_name[1] is '':
+                image_path = 'images/' + file_name[0] + '.jpg'
+            else:
+                image_path = 'images/' + file_name[0] + file_name[1]
+
+            item.image_location = image_path
+
+            if os.path.exists(image_path):
+                print("File :" + image_path + " already exists")
+            else:
+                urllib.request.urlretrieve(item.image_url, image_path)
+                print("File savepath:" + image_path)
 
     return True
