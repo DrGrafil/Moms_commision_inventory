4bacc92333293e099c74ed09b1b80a5eaf8c268c
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 22:35:24 -0800

    Update write_latex.py

--- write_latex.py
+++ write_latex.py
@@ -4,21 +4,28 @@ from Inventory_Item import Inventory_Item
 def write_latex(all_inventory: [Inventory_Item]) -> bool:
 
     with open('latex/inventory.tex', 'w') as f:
-        f.write('\\documentclass{article}\n')
+        f.write('\\documentclass{report}\n')
+        f.write('\\usepackage[margin=0.5in]{geometry}\n')
         f.write('\\usepackage{multirow}\n')
         f.write('\\usepackage{graphicx}\n')
         f.write('\\begin{document}\n')
 
+        item_counter = 0
         for item in all_inventory:
+            item_counter += 1
             f.write('\n')
             f.write('\\begin{table}[]\n')
-            f.write('\\begin{tabular}{lllll}\n')
-            f.write('\\multicolumn{5}{l}{'+ item.name +'}\\\\ \n')
-            f.write('\\multirow{3}{*}{\\includegraphics[width=5cm, height=5cm]{./../' + item.image_location +'}} & Price: ' + item.price + ' & Commission Rate: ' + item.commission_rate + ' & Commission Amount: '+ item.commission_amount +' & \\\\ \n')
-            f.write('& \\multicolumn{3}{l}{\\multirow{2}{*}{' + "description here" + '}} & Date Recieved: ' + item.received_date + ' \\\\ \n') #item.description
-            f.write('& \\multicolumn{3}{l}{} & Date Sale: ' + item.sale_date + '\n')
+            f.write('\\begin{tabular}{llllllll}\n')
+            f.write('\\multicolumn{8}{l}{' + item.name + '}\\\\ \n')
+            f.write('\\multirow{3}{*}{\\includegraphics[width=3cm, height=3cm]{./../' + item.image_location + '}} & Price & Commission Rate & Commission Amount & Recieved & Sale \\\\ \n')
+            f.write(' & ' + item.price + ' & ' + item.commission_rate + ' & ' + item.commission_amount + ' & ' + item.received_date + ' & ' + item.sale_date + ' \\\\ \n')
+            f.write(' & \\multicolumn{3}{l}{\\multirow{2}{*}{' + "description here" + '}} &  \\\\ \n') #item.description
             f.write('\\end{tabular}\n')
             f.write('\\end{table}\n')
+
+            if item_counter is 5:
+                f.write('\n\\clearpage\n')
+                item_counter = 0
 
         f.write('\\end{document}\n')
     return True
