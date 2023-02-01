30feee7cd49b847b43e5f877b0479fb4abf21ab4
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 22:05:07 -0800

    Update write_latex.py

--- write_latex.py
+++ write_latex.py
@@ -2,9 +2,24 @@ from Inventory_Item import Inventory_Item
 
 
 def write_latex(all_inventory: [Inventory_Item]) -> bool:
-    for item in all_inventory:
-        print(item.image_url)
-        #urllib.request.urlretrieve()
 
+    with open('latex/inventory.tex', 'w') as f:
+        f.write('\\documentclass{article}\n')
+        f.write('\\usepackage{multirow}\n')
+        f.write('\\usepackage{graphicx}\n')
+        f.write('\\begin{document}\n')
+
+        for item in all_inventory:
+            f.write('\n')
+            f.write('\\begin{table}[]\n')
+            f.write('\\begin{tabular}{lllll}\n')
+            f.write('\\multicolumn{5}{l}{'+ item.name +'}\\\\ \n')
+            f.write('\\multirow{3}{*}{\\includegraphics[width=5cm, height=5cm]{./../' + item.image_location +'}} & Price: ' + item.price + ' & Commission Rate: ' + item.commission_rate + ' & Commission Amount: '+ item.commission_amount +' & \\\\ \n')
+            f.write('& \\multicolumn{3}{l}{\\multirow{2}{*}{' + "description here" + '}} & Date Recieved: ' + item.received_date + ' \\\\ \n') #item.description
+            f.write('& \\multicolumn{3}{l}{} & Date Sale: ' + item.sale_date + '\n')
+            f.write('\\end{tabular}\n')
+            f.write('\\end{table}\n')
+
+        f.write('\\end{document}\n')
     return True
 
