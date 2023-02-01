a93cd158b0a83ccac8d67e3939d34ef5a70e5ba3
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 21:01:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-
-
+from read_inventory_csv import read_inventory_csv
+from grab_images import grab_images
+from write_latex import write_latex
 
 def main():
     inventory = read_inventory_csv()
-    grab_images(inventory.image_list)
+    grab_images(inventory)
 
     write_latex(inventory)
 
