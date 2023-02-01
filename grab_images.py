32b78d7e39c7c24df7dbc90d212733d51af61ad1
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 21:01:50 -0800

    Update grab_images.py

--- grab_images.py
+++ grab_images.py
@@ -1,30 +1,10 @@
+import urllib.request
+from Inventory_Item import Inventory_Item
 
 
+def grab_images(all_inventory: [Inventory_Item]) -> bool:
+    for item in all_inventory:
+        print(item.image_url)
+        #urllib.request.urlretrieve()
 
-
-class Inventory:
-    def __init__(self):
-        self.field_name = None
-        self.dicom_data = None
-
-        """MAGIC NUMBERS"""
-        self.SID = 1.5
-        self.BB_Count = 5
-
-        """Note DRR is the DRR.(Name) ie the FieldNameInput slice of DRR"""
-        self.DRR = None
-        self.DRR_Box_Centers = None
-        self.DRR_BB_Centers = None
-        self.DRR_BB_Radii = None
-        self.DRR_BB_Sensitivity = None
-        self.DRR_BB_EdgeThreshold = None
-
-        """Note Portal is the Portal.(Name) ie the FieldNameInput slice of portal"""
-        self.Portal = None
-        self.Portal_Box_Centers = None
-        self.Portal_BB_Centers = None
-        self.Portal_BB_Radii = None
-        self.Portal_BB_Sensitivity = None
-        self.Portal_BB_EdgeThreshold = None
-
-        self.MLC_Apertures = None
+    return True
