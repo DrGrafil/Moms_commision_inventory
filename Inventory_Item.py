bfe98e92342945f62c1b6eb0991af14e9229d55e
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-01-31 20:51:17 -0800

    Inventory item

---
+++ Inventory_Item.py
@@ -0,0 +1,20 @@
+
+
+
+class Inventory_Item:
+    def __init__(self):
+        self.sku = None
+        self.vendor_sku = None
+
+        """data"""
+        self.name = None
+        self.description = None
+        self.price = None
+        self.status = None
+        self.commission_rate = None
+        self.commission_amount = None
+        self.received_date = None
+        self.sale_date = None
+
+        self.image_url = None
+        self.image_location = None
