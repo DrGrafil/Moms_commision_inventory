c5c6fed6127fe352a763bd909b43259ab786fc45
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-02-02 20:35:57 -0800

    mom code

--- grab_site_sku_images.py
+++ grab_site_sku_images.py
@@ -1,5 +1,6 @@
 import os
 import urllib.request
+from urllib.error import HTTPError
 from urllib.parse import urlparse
 from pathlib import Path
 from Inventory_Item import Inventory_Item
@@ -15,14 +16,19 @@ def grab_site_sku_images(all_inventory: [Inventory_Item]) -> bool:
             print("File: " + file_name)
             image_path = 'images_sku/' + file_name
 
-            item.image_location = image_path
-
             if os.path.exists(image_path):
                 print("File :" + image_path + " already exists")
+                item.image_location = image_path
             else:
                 "https://product-images.therealreal.com/BRU218318_1_product.jpg"
                 url_path = "https://product-images.therealreal.com/" + file_name
-                urllib.request.urlretrieve(url_path, image_path)
-                print("File savepath:" + image_path)
+                try:
+                    urllib.request.urlretrieve(url_path, image_path)
+                    print("File savepath:" + image_path)
+                    item.image_location = image_path
+                except HTTPError as e:
+                    print("No sku image: " + url_path)
+                    item.image_location = 'images_sku/no_image.jpg'
 
     return True
+
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from grab_site_sku_images import grab_site_sku_images
 def main():
     inventory = read_inventory_csv()
 
-    use_personal_images = True
+    use_personal_images = False
     if use_personal_images:
         grab_images(inventory)
     else:
