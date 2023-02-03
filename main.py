e1db1b158564d448fd94bd5fa4c078144fb86e11
ElliotLucaMedCo <56279554+ElliotLucaMedCo@users.noreply.github.com>
2023-02-02 20:27:55 -0800

    changes to image grabbing based on sku

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from read_inventory_csv import read_inventory_csv
 from grab_images import grab_images
 from write_latex import write_latex
+from grab_site_sku_images import grab_site_sku_images
+
 
 def main():
     inventory = read_inventory_csv()
-    grab_images(inventory)
+
+    use_personal_images = True
+    if use_personal_images:
+        grab_images(inventory)
+    else:
+        grab_site_sku_images(inventory)
 
     write_latex(inventory)
 
